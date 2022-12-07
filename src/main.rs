2149260875cbef12f602a41bbfd9ff867afd0074
marc2332 <mespinsanz@gmail.com>
2022-12-07 17:37:44 +0100

    feat: Improve code

--- src/main.rs
+++ src/main.rs
@@ -14,113 +14,146 @@ fn main() {
     )]);
 }
 
+#[derive(Clone)]
+pub enum SyntaxType {
+    Number,
+    String,
+    Keyword,
+    Operator,
+    Variable,
+    Unknown,
+}
+
+impl From<&str> for SyntaxType {
+    fn from(s: &str) -> Self {
+        match s {
+            "keyword" => SyntaxType::Keyword,
+            "variable" => SyntaxType::Variable,
+            "operator" => SyntaxType::Operator,
+            "string" => SyntaxType::String,
+            "number" => SyntaxType::Number,
+            _ => SyntaxType::Unknown,
+        }
+    }
+}
+
+type SyntaxBlocks = Vec<Vec<(SyntaxType, String)>>;
+
+fn use_syntax_highlighter<'a>(
+    cx: &'a ScopeState,
+    content: &EditableText,
+) -> &'a UseState<SyntaxBlocks> {
+    let syntax_blocks = use_state::<SyntaxBlocks>(cx, Vec::new);
+    let highlighter = cx.use_hook(Highlighter::new);
+
+    use_effect(cx, &content.len(), move |_| {
+        let highlight_names = &mut [
+            "attribute",
+            "constant",
+            "function.builtin",
+            "function",
+            "keyword",
+            "operator",
+            "property",
+            "punctuation",
+            "punctuation.bracket",
+            "punctuation.delimiter",
+            "string",
+            "string.special",
+            "tag",
+            "type",
+            "type.builtin",
+            "variable",
+            "variable.builtin",
+            "variable.parameter",
+            "number",
+        ];
+
+        let mut javascript_config = HighlightConfiguration::new(
+            tree_sitter_javascript::language(),
+            tree_sitter_javascript::HIGHLIGHT_QUERY,
+            tree_sitter_javascript::INJECTION_QUERY,
+            tree_sitter_javascript::LOCALS_QUERY,
+        )
+        .unwrap();
+
+        javascript_config.configure(highlight_names);
+
+        let data = content.to_string();
+        let highlights = highlighter
+            .highlight(&javascript_config, data.as_bytes(), None, |_| None)
+            .unwrap();
+
+        syntax_blocks.with_mut(|syntax_blocks| {
+            syntax_blocks.clear();
+            let mut prepared_block: (SyntaxType, Vec<(usize, String)>) =
+                (SyntaxType::Unknown, vec![]);
+
+            for event in highlights {
+                match event.unwrap() {
+                    HighlightEvent::Source { start, end } => {
+                        // Prepare the whole block even if it's splitted across multiple lines.
+                        let data = content.get().lines(start..end);
+                        let starting_line = content.get().line_of_offset(start);
+
+                        for (i, d) in data.enumerate() {
+                            prepared_block.1.push((starting_line + i, d.to_string()));
+                        }
+                    }
+                    HighlightEvent::HighlightStart(s) => {
+                        // Specify the type of the block
+                        prepared_block.0 = SyntaxType::from(highlight_names[s.0]);
+                    }
+                    HighlightEvent::HighlightEnd => {
+                        // Push all the block chunks to their specified line
+                        for (i, d) in prepared_block.1 {
+                            if syntax_blocks.get(i).is_none() {
+                                syntax_blocks.push(vec![]);
+                            }
+                            let line = syntax_blocks.last_mut().unwrap();
+                            line.push((prepared_block.0.clone(), d));
+                        }
+                        // Clear the prepared block
+                        prepared_block = (SyntaxType::Unknown, vec![]);
+                    }
+                }
+            }
+
+            // Mark all the remaining text as not readable
+            if !prepared_block.1.is_empty() {
+                for (i, d) in prepared_block.1 {
+                    if syntax_blocks.get(i).is_none() {
+                        syntax_blocks.push(vec![]);
+                    }
+                    let line = syntax_blocks.last_mut().unwrap();
+                    line.push((SyntaxType::Unknown, d));
+                }
+            }
+        });
+
+        async move {}
+    });
+
+    syntax_blocks
+}
+
+const DUMMY_CODE: &str = "const test = false;\n\nfunction test(val = 123){\n   console.log(val);\n   let data = `multi\n   line`;\n}\n\n";
+
 #[allow(non_snake_case)]
 fn Body(cx: Scope) -> Element {
-    let dummy_code = cx.use_hook(||"const test = false;\nlet data = `multi\nline`;\nfunction test(val = 123){\n   console.log(val);\n}\n\n".repeat(250));
+    let dummy_code = cx.use_hook(|| DUMMY_CODE.repeat(400));
     let theme = use_theme(&cx);
-    let theme = theme.read();
-    let (content, cursor, process_keyevent, process_clickevent, cursor_ref) =
-        use_editable(&cx, || dummy_code, EditableMode::SingleLineMultipleEditors);
     let font_size_percentage = use_state(&cx, || 15.0);
     let line_height_percentage = use_state(&cx, || 0.0);
     let is_bold = use_state(&cx, || false);
     let is_italic = use_state(&cx, || false);
 
-    // minimum font size is 5
+    let (content, cursor, process_keyevent, process_clickevent, cursor_ref) =
+        use_editable(&cx, || dummy_code, EditableMode::SingleLineMultipleEditors);
+    let syntax_blocks = use_syntax_highlighter(&cx, content);
+
     let font_size = font_size_percentage + 5.0;
     let line_height = (line_height_percentage / 25.0) + 1.2;
-
-    let words = use_state::<Vec<Vec<(&str, String)>>>(&cx, || vec![]);
-
-    use_effect(&cx, &content.len(), move |_| {
-        let content = content.clone();
-        let words = words.clone();
-        async move {
-            let content = content.clone();
-            let highlight_names = &mut [
-                "attribute",
-                "constant",
-                "function.builtin",
-                "function",
-                "keyword",
-                "operator",
-                "property",
-                "punctuation",
-                "punctuation.bracket",
-                "punctuation.delimiter",
-                "string",
-                "string.special",
-                "tag",
-                "type",
-                "type.builtin",
-                "variable",
-                "variable.builtin",
-                "variable.parameter",
-                "number",
-            ];
-
-            let mut highlighter = Highlighter::new();
-
-            let mut javascript_config = HighlightConfiguration::new(
-                tree_sitter_javascript::language(),
-                tree_sitter_javascript::HIGHLIGHT_QUERY,
-                tree_sitter_javascript::INJECTION_QUERY,
-                tree_sitter_javascript::LOCALS_QUERY,
-            )
-            .unwrap();
-
-            javascript_config.configure(highlight_names);
-
-            let data = content.to_string();
-            let highlights = highlighter
-                .highlight(&javascript_config, data.as_bytes(), None, |_| None)
-                .unwrap();
-
-            words.with_mut(|words| {
-                words.clear();
-
-                let mut last_finished: (Option<&str>, Vec<(usize, String)>) = (None, vec![]);
-
-                for event in highlights {
-                    match event.unwrap() {
-                        HighlightEvent::Source { start, end } => {
-                            let data = content.get().lines(start..end);
-                            let starting_line = content.get().line_of_offset(start);
-
-                            for (i, d) in data.enumerate() {
-                                last_finished.1.push((starting_line + i, d.to_string()));
-                            }
-                        }
-                        HighlightEvent::HighlightStart(s) => {
-                            last_finished.0 = Some(highlight_names[s.0]);
-                        }
-                        HighlightEvent::HighlightEnd => {
-                            for (i, d) in last_finished.1 {
-                                if words.get(i).is_none() {
-                                    words.push(vec![]);
-                                }
-                                let line = words.last_mut().unwrap();
-                                line.push((last_finished.0.unwrap(), d));
-                            }
-                            last_finished = (None, vec![]);
-                        }
-                    }
-                }
-
-                // Mark all the remaining text as not readable
-                if !last_finished.1.is_empty() {
-                    for (i, d) in last_finished.1 {
-                        if words.get(i).is_none() {
-                            words.push(vec![]);
-                        }
-                        let line = words.last_mut().unwrap();
-                        line.push(("", d));
-                    }
-                }
-            });
-        }
-    });
-
     let font_style = {
         if *is_bold.get() && *is_italic.get() {
             "bold-italic"
@@ -132,7 +165,7 @@ fn Body(cx: Scope) -> Element {
             "normal"
         }
     };
-
+    let theme = theme.read();
     let manual_line_height = (font_size * line_height) as f32;
 
     render!(
@@ -258,14 +291,14 @@ fn Body(cx: Scope) -> Element {
                     width: "100%",
                     height: "100%",
                     show_scrollbar: true,
-                    builder_values: (cursor, words),
-                    length: words.len() as i32,
+                    builder_values: (cursor, syntax_blocks),
+                    length: syntax_blocks.len() as i32,
                     item_size: manual_line_height,
                     builder: Box::new(move |(k, line_index, args)| {
-                        let (cursor, words) = args.unwrap();
+                        let (cursor, syntax_blocks) = args.unwrap();
                         let process_clickevent = process_clickevent.clone();
                         let line_index = line_index as usize;
-                        let line = words.get().get(line_index).as_ref().unwrap().clone();
+                        let line = syntax_blocks.get().get(line_index).unwrap().clone();
 
                         let is_line_selected = cursor.1 == line_index;
 
@@ -353,13 +386,13 @@ fn app(cx: Scope) -> Element {
     render!(Body {})
 }
 
-fn get_color_from_type(t: &str) -> &str {
+fn get_color_from_type<'a>(t: &SyntaxType) -> &'a str {
     match t {
-        "keyword" => "rgb(248, 73, 52)",
-        "variable" => "rgb(189, 174, 147)",
-        "operator" => "rgb(189, 174, 147)",
-        "string" => "rgb(184, 187, 38)",
-        "number" => "rgb(211, 134, 155)",
-        _ => "rgb(189, 174, 147)",
+        SyntaxType::Keyword => "rgb(248, 73, 52)",
+        SyntaxType::Variable => "rgb(189, 174, 147)",
+        SyntaxType::Operator => "rgb(189, 174, 147)",
+        SyntaxType::String => "rgb(184, 187, 38)",
+        SyntaxType::Number => "rgb(211, 134, 155)",
+        SyntaxType::Unknown => "rgb(189, 174, 147)",
     }
 }
